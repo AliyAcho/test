51054ae85285c67c128a83d8de1577166b2d5fab
AliyAcho <rita.achmizova@mail.ru>
2022-06-18 23:07:24 +0300

    README.md update

--- app/core/management/commands/go.py
+++ app/core/management/commands/go.py
@@ -5,14 +5,10 @@ import time
 
 class Command(BaseCommand):
 
-
     def handle(self, *args, **options):
         while True:
             try:
                 update_data()
-                self.stdout.write(self.style.SUCCESS('Good'))
                 time.sleep(60)
             except:
-                self.stdout.write(self.style.SUCCESS('Bad'))
                 time.sleep(60)
-                
